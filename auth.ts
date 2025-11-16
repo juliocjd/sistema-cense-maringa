aae48a6b2d77f6583b95dec1a7f4a675d2acb853
Julio Justini <julio.justini@gmail.com>
2025-11-16 18:05:26 -0300

    correção do logout

--- auth.ts
+++ auth.ts
@@ -6,6 +6,8 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
 import bcrypt from "bcryptjs";
 
+export const runtime = "nodejs";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   trustHost: true,
   adapter: PrismaAdapter(prisma),
